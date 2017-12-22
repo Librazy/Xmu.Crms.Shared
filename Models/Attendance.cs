436d556f7ba9a0e4a757cac1d62f42a452730555
Librazy <im@librazy.org>
2017-12-22 23:53:04 +0800

    update std

--- Models/Attendance.cs
+++ Models/Attendance.cs
@@ -12,13 +12,10 @@ namespace Xmu.Crms.Shared.Models
     public class Attendance
     {
         public long Id { get; set; }
-
         [ForeignKey("student_id")]
         public UserInfo Student { get; set; }
-
         [ForeignKey("class_id")]
         public ClassInfo ClassInfo { get; set; }
-
         public AttendanceStatus AttendanceStatus { get; set; }
     }
 }
